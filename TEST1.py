b6f203c9d1d4eac186547e0138ed31cc5ee2104f
Caleb Keller <caleb.keller@tooeleschools.org>
2020-11-03 10:15:06 -0700

    add files

---
+++ TEST1.py
@@ -0,0 +1,26 @@
+#OREGAN TRAIL
+#Anthony Garrard // Caleb Keller
+#started 11/20
+def LogoScreen():
+  """Displays Logo, Names, and copyright"""
+  logo = """
+                  _____________
+                 |   ___    __/   
+                 |  |   |  |    
+                 |  |   |  |
+                 |  |   |  |___________________
+                 |  |___|          _________   |
+                 |      ________  |_________|  |
+        __       |     |        |     ______   | 
+       |  \______|     |        |    |      \\__|
+       |   _________   |________|    |   
+       |  |_________|          ___   |     
+       |___________________   |   |  |                           
+                           |  |   |  |    
+                           |  |   |  |
+                         __|  |___|  |
+                        \\____________| """
+  names = "Anthony // Caleb"
+  cright = "©Shuriken Studios"
+  print(logo, names, cright, sep="\n")
+LogoScreen()
