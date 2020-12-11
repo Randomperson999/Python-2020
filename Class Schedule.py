196cdc164a26758aacd7e9ed28c32fda614756b9
Randomperson999 <arandomguy9638@gmail.com>
2020-12-11 14:10:39 -0700

    added more files

---
+++ Class Schedule.py
@@ -0,0 +1,9 @@
+#Name: Caleb Keller
+#Date: 9/15/2020
+#Periods: 1st & 2nd
+#This project is to show what classes I have and what times.
+print("Red Day:")
+print("8:00 (1st-2nd): Python Programming")
+print("10:30 (3rd): Weights")
+print("12:15 (4th): US History")
+print("1:30 (5th): Chinese III")
