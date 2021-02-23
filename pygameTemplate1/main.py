ab8b9f772183387e01c7271574e35f2fcf530d4f
Caleb Keller <caleb.keller@tooeleschools.org>
2021-02-23 10:16:05 -0700

    add

---
+++ pygameTemplate1/main.py
@@ -0,0 +1,28 @@
+# Caleb Keller
+# Pygame Starter Template
+# 2/23/2021
+
+import pygame
+import random
+
+HEIGHT = 400
+WIDTH = 400
+FPS = 30
+title = "Template"
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(title)
+
+clock = pygame.time.Clock()
+
+# game loop
+while True:
+    # process input
+
+    # update events
+
+    # render all changes
